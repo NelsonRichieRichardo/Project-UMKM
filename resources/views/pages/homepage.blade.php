bbcf429abf2d32a3ad6b3666d5a49cec48df92e5
NelsonRichieRichardo <nelsonrichierichardo@gmail.com>
2024-04-29 20:23:01 +0700

    Added Tailwind CSS

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Home | De'Sate</title>
 </head>
 <body>
+    <header>
+        <div>
+            <div>
+                Home
+            </div>
+            <div>
+                Menu
+            </div>
+            <div>
+                About
+            </div>
+            <div>
+                Catalog
+            </div>
+        </div>
+    </header>
     <h1>Home</h1>
 </body>
 </html>
