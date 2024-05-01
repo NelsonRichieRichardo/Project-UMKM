a90464a1f56d2c6803211117bf3563bf56f5d3d1
jovansklh <jovansklh29@gmail.com>
2024-05-01 14:57:41 +0700

    testpush

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -22,6 +22,9 @@
             <div class="basis-1/4">
                 Catalog
             </div>
+            <div class="basis-1/4">
+                TEST
+            </div>
         </div>
     </header>
     <h1 class="">Home</h1>
