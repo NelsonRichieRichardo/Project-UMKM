1438ac9b56bfe1e44a57e5d02c4fcdcb103fb1bd
RudiPangestu <rudipangsentu@gmail.com>
2024-05-01 18:34:48 +0700

    RUdiPangestu

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -8,33 +8,19 @@
     <title>Home | De'Sate</title>
 </head>
 <body>
-    <nav class="nav-header">
-        <div class="nav-option">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="/catalog">Catalog</a></li>
-            </ul>
-        </div>
+    <nav class="nav">
+      <div class="nav-item">Home</div>
+      <div class="nav-item">Menu</div>
+      <div class="nav-item">About</div>
+      <div class="nav-item">Catalog</div>
     </nav>
-
-    <section id="main" class="container size-full">
-        <div id="main-flex" class="flex flex-row"> 
-            <div id="store-name-flex" class="flex-none items-center w-[40%] h-[60vh] grid grid-row auto-row-max content-center">
-                <h1 class="font-sans text-7xl font-bold">De' Sate</h1>
-                <div class="indent-9"><h2 class="font-sans text-2xl ">Your cheapest and tastiest food options</h2></div>
-                <div class="indent-9"><h2 class="font-sans text-2xl ">are from here.</h2></div>
-            </div>
-            <div id="products-flex" class="flex-initail w-[60%] align-center">
-                <img alt="image"><img>
-            </div>
-        </div>
+    <section class="section">
+      <div class="title">De'Sate</div>
+      <div class="subtitle">Your cheapest & tastiest food options are from here.</div>
     </section>
-    <section id="top-picks" class="">
-
-    </section>
-    <section id="front-store" class=""></section>
-
-</body>
+    <div class="menu-item">
+        <img src="spaghetti-meatball.jpg" alt="Spaghetti Meatball">
+        <span>Spaghetti Meatball</span>
+    </div>
+  </body>
 </html>
