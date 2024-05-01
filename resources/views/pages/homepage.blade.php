d1d8e22f2cdedb49d9de71a9725b4a56fb7de7b2
NelsonRichieRichardo <nelsonrichierichardo@gmail.com>
2024-05-01 16:02:17 +0700

    homepage -nelson

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -22,17 +22,6 @@
             <div class="basis-1/4">
                 Catalog
             </div>
-            <div class="basis-1/4">
-                TEST
-            </div>
-            <div class="basis-1/4">
-                TEST2
-            </div>
-                TEST
-            </div>
-            <div class="basis-1/4">
-                TEST
-            </div>
         </div>
     </header>
     <h1 class="">Home</h1>
