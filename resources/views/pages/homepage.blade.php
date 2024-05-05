2c4f88f0271e02ef9adeb2ec6f80546e463d0c56
RudiPangestu <rudipangsentu@gmail.com>
2024-05-05 11:49:39 +0700

    rudi

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -32,7 +32,7 @@
             height: 100px;
             position: relative;
             top: 110px;
-            left : 480px;
+            left : 465px;
             display : flex;
             justify-content : space-between
         }
@@ -55,7 +55,7 @@
             </div>
         </nav>
         <div class="sampingan">
-            <img class="samping" src="build/assets/miegor.jpg" alt="miegor" width="150px" height="100px">
+            <img class="samping" src="build/assets/miegor.jpg" alt="miegor" width="200px" height="170px">
             <img class="samping" src="build/assets/ayamp.jpg" alt="ayamp" width="150px" height="100px">
         </div>
 
