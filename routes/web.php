23a8b8621d7baeb97a073d6e618a775a5e1d6fd1
NelsonRichieRichardo <nelsonrichierichardo@gmail.com>
2024-05-05 13:02:08 +0700

    login -nelson

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,4 +10,8 @@ Route::get('/', function () {
 
 Route::get('/home', [PagesController::class, 'home'])->name('homepage');
 Route::get('/catalog', [PagesController::class, 'catalog'])->name('catalog');
-Route::get('/login', [PagesController::class, 'login'])->name('login');
+
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::get('/admin', [PagesController::class, 'admin'])->name('admin');
+
